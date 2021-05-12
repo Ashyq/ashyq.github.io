d4cd61cd50857e792d76a88022c236fe712a85ca
Ashyq <84073534+Ashyq@users.noreply.github.com>
2021-05-13 04:02:49 +0600

    Add files via upload
    two functions

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ window.onload = function () {
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
+
+//second function
+
+   function digitalClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+       //* добавление ведущих нулей */
+      if (hours < 10) hours = "0" + hours;
+      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;
+        document.getElementById("id_clock").innerHTML = hours + ":" + minutes;
+        setTimeout("digitalClock()", 1000);
+   }
